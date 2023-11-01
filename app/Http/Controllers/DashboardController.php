6a3c127ced4d5df7943a1dcd7196184ef7cd7cd6
Calvin Nugraha <calvinnugraha5@gmail.com>
2023-11-01 12:55:58 +0700

    cn

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Kendaraan;
+use App\Models\Elektronik;
+use App\Models\Furnitur;
+use App\Models\Aksesori;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request)
+        public function index(Request $request)
     {
-        $kendaraans = Kendaraan::all();
-
         $kendaraans = Kendaraan::latest()->get();
-
-        return view('dashboard.index', compact('kendaraans'));
+        $elektroniks = Elektronik::latest()->get();
+        $furniturs = Furnitur::latest()->get();
+        $aksesoris = Aksesori::latest()->get();
+    
+        return view('dashboard.index', compact('kendaraans', 'elektroniks', 'furniturs', 'aksesoris'));
     }
+
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -91,8 +91,9 @@
             <div class="col">
                 <h1 class="text-center" style="margin-bottom: 3rem">Dashboard</h1>
                 <div class="col" style="margin-left: 32px">
-                    <table class="table table-hover table-striped text-center" id="kwitansi-table"
+                    <table class="table table-hover table-striped text-center" id="kendaraan-table"
                         style="margin-bottom: 2rem">
+                        <caption class="text-center" style="caption-side: top; font-size: 1.5em; margin-bottom: 10px;">Kendaraan</caption>
                         <thead>
                             <tr class="bg-info">
                                 <th style="width: 1.5rem; justify-content: center; align-items: center; cursor: pointer; border-top-left-radius:6px "
@@ -122,6 +123,108 @@
                                     <td>{{ $kendaraan->lama_pakai }}</td>
                                     <td>{{ $kendaraan->lokasi }}</td>
                                     <td>{{ $kendaraan->masa_pajak }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <table class="table table-hover table-striped text-center" id="elektronik-table"
+                        style="margin-bottom: 2rem">
+                        <caption class="text-center" style="caption-side: top; font-size: 1.5em; margin-bottom: 10px;">Elektronik</caption>
+                        <thead>
+                            <tr class="bg-info">
+                                <th style="width: 1.5rem; justify-content: center; align-items: center; cursor: pointer; border-top-left-radius:6px "
+                                    id="sortNo">No.</th>
+                                <th style="width: 5rem">Nama Admin</th>
+                                <th style="width: 3rem">Tanggal</th>
+                                <th style="width: 4rem">Kode</th>
+                                <th style="width: 4rem">Jenis Elektronik</th>
+                                <th style="width: 4rem">Kondisi</th>
+                                <th style="width: 4rem">Masa Guna</th>
+                                <th style="width: 4rem">Lama Pakai</th>
+                                <th style="width: 4rem">Lokasi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($elektroniks as $elektronik)
+                                <tr onclick="window.location.href='{{ route('elektronik.detail', $elektronik->id) }}';"
+                                    style="cursor: pointer;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ optional($elektronik->user)->name }}</td>
+                                    <td>{{ date('j F Y', strtotime($elektronik->created_at)) }}</td>
+                                    <td>{{ $elektronik->kode }}</td>
+                                    <td>{{ $elektronik->jenis_elektronik }}</td>
+                                    <td>{{ $elektronik->kondisi }}</td>
+                                    <td>{{ $elektronik->masa_guna }}</td>
+                                    <td>{{ $elektronik->lama_pakai }}</td>
+                                    <td>{{ $elektronik->lokasi }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <table class="table table-hover table-striped text-center" id="furnitur-table"
+                        style="margin-bottom: 2rem">
+                        <caption class="text-center" style="caption-side: top; font-size: 1.5em; margin-bottom: 10px;">Furnitures</caption>
+                        <thead>
+                            <tr class="bg-info">
+                                <th style="width: 1.5rem; justify-content: center; align-items: center; cursor: pointer; border-top-left-radius:6px "
+                                    id="sortNo">No.</th>
+                                <th style="width: 5rem">Nama Admin</th>
+                                <th style="width: 3rem">Tanggal</th>
+                                <th style="width: 4rem">Kode</th>
+                                <th style="width: 4rem">Jenis Furnitures</th>
+                                <th style="width: 4rem">Kondisi</th>
+                                <th style="width: 4rem">Masa Guna</th>
+                                <th style="width: 4rem">Lama Pakai</th>
+                                <th style="width: 4rem">Lokasi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($furniturs as $furnitur)
+                                <tr onclick="window.location.href='{{ route('furnitur.detail', $furnitur->id) }}';"
+                                    style="cursor: pointer;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ optional($furnitur->user)->name }}</td>
+                                    <td>{{ date('j F Y', strtotime($furnitur->created_at)) }}</td>
+                                    <td>{{ $furnitur->kode }}</td>
+                                    <td>{{ $furnitur->jenis_furniture }}</td>
+                                    <td>{{ $furnitur->kondisi }}</td>
+                                    <td>{{ $furnitur->masa_guna }}</td>
+                                    <td>{{ $furnitur->lama_pakai }}</td>
+                                    <td>{{ $furnitur->lokasi }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <table class="table table-hover table-striped text-center" id="aksesori-table"
+                        style="margin-bottom: 2rem">
+                        <caption class="text-center" style="caption-side: top; font-size: 1.5em; margin-bottom: 10px;">Aksesoris</caption>
+                        <thead>
+                            <tr class="bg-info">
+                                <th style="width: 1.5rem; justify-content: center; align-items: center; cursor: pointer; border-top-left-radius:6px "
+                                    id="sortNo">No.</th>
+                                <th style="width: 5rem">Nama Admin</th>
+                                <th style="width: 3rem">Tanggal</th>
+                                <th style="width: 4rem">Kode</th>
+                                <th style="width: 4rem">Jenis Aksesoris</th>
+                                <th style="width: 4rem">Kondisi</th>
+                                <th style="width: 4rem">Masa Guna</th>
+                                <th style="width: 4rem">Lama Pakai</th>
+                                <th style="width: 4rem">Lokasi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($aksesoris as $aksesori)
+                                <tr onclick="window.location.href='{{ route('aksesori.detail', $aksesori->id) }}';"
+                                    style="cursor: pointer;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ optional($aksesori->user)->name }}</td>
+                                    <td>{{ date('j F Y', strtotime($aksesori->created_at)) }}</td>
+                                    <td>{{ $aksesori->kode }}</td>
+                                    <td>{{ $aksesori->jenis_aksesoris }}</td>
+                                    <td>{{ $aksesori->kondisi }}</td>
+                                    <td>{{ $aksesori->masa_guna }}</td>
+                                    <td>{{ $aksesori->lama_pakai }}</td>
+                                    <td>{{ $aksesori->lokasi }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
